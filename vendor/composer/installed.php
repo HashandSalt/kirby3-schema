64cb8af9647699d0944b90a99559043fe65549fd
Hash&Salt <hello@hashandsalt.com>
2021-11-20 13:26:44 +0000

    Upgrade schema-org

---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'kirby-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'c79b430a10bbb506915641dd7bef5cd5b469ae3b',
+        'name' => 'hashandsalt/kirby3-schema',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'getkirby/composer-installer' => array(
+            'pretty_version' => '1.2.1',
+            'version' => '1.2.1.0',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/../getkirby/composer-installer',
+            'aliases' => array(),
+            'reference' => 'c98ece30bfba45be7ce457e1102d1b169d922f3d',
+            'dev_requirement' => false,
+        ),
+        'hashandsalt/kirby3-schema' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'kirby-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'c79b430a10bbb506915641dd7bef5cd5b469ae3b',
+            'dev_requirement' => false,
+        ),
+        'spatie/schema-org' => array(
+            'pretty_version' => '3.8.0',
+            'version' => '3.8.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../spatie/schema-org',
+            'aliases' => array(),
+            'reference' => 'ce9c9ed51b0c0f9cee682e5a8823bddcc1ceae04',
+            'dev_requirement' => false,
+        ),
+    ),
+);
